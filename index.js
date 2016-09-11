c415c2fde0f00682cad1af1d3e203610c85d06d3
Guillaume Verbal <guillaume.verbal@gmail.com>
2016-09-11 17:06:05 +0800

    brackets are now compliant with airbnb code style

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ program
  * Host and port sanitizing
  */
 let host = '127.0.0.1';
-if (program.args[0])
-{
+if (program.args[0]) {
     // if (!validator.isIP(program.args[0]))
     //     return console.error('<host> should be a valid IP address.')
     host = program.args[0];
 }
 
 let port = 9200;
-if (program.args[1])
-{
-    if (!validator.isInt(program.args[1], { min: 0, max: 0xFFFF }))
+if (program.args[1]) {
+    if (!validator.isInt(program.args[1], { min: 0, max: 0xFFFF })) {
         return console.error('<port> should be a valid port number.');
+    }
     
     port = program.args[1];
 }
@@ -36,12 +35,12 @@ global.ES_PORT = port;
 /*
  * ES version support checks
  */
-helper.getESVersion((error, version) =>
-{
-    if (error)
-        return helper.failAndExit(error);
-    if (2 != version.major)
+helper.getESVersion((error, version) => {
+    if (error) return helper.failAndExit(error);
+
+    if (2 != version.major) {
         return helper.failAndExit(new Error('This module only supports ES 2.x.'));
+    }
 
     require('./lib/2_x').start(version);
 });
